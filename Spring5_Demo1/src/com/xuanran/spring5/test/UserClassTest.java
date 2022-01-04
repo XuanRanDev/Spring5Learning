957ddb01eb08f4fed4810b4a67a2d774ca60518d
XuanRanDev <3135419729@qq.com>
2022-01-04 14:39:53 +0800

    Add user class test class.

---
+++ Spring5_Demo1/src/com/xuanran/spring5/test/UserClassTest.java
@@ -0,0 +1,14 @@
+package com.xuanran.spring5.test;
+
+import org.junit.Test;
+
+/**
+ * Created By XuanRan on 2022/1/4
+ */
+public class UserClassTest {
+
+    @Test
+    public void TestAdd(){
+
+    }
+}
