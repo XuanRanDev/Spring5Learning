657484b33cfea59ef6128909f8216dde2af7a3b1
XuanRanDev <3135419729@qq.com>
2022-01-04 14:47:51 +0800

    完成测试类开发

--- Spring5_Demo1/src/com/xuanran/spring5/test/UserClassTest.java
+++ Spring5_Demo1/src/com/xuanran/spring5/test/UserClassTest.java
@@ -1,6 +1,9 @@
 package com.xuanran.spring5.test;
 
+import com.xuanran.spring5.User;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created By XuanRan on 2022/1/4
@@ -9,6 +12,9 @@ public class UserClassTest {
 
     @Test
     public void TestAdd(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("UserBeanConfig.xml");
+        User user = context.getBean("user",User.class);
+        user.add();
 
     }
 }
